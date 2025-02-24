f22fb897226cfb99ad3e3e66edc43f14f59dc08d
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-24 19:46:15 +0100

    Started building the home screen ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,46 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: _buildUI(context));
+  }
+
+  Widget _buildUI(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.sizeOf(context).width,
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.sizeOf(context).width * 0.2,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [_allPokemonsList(context)],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _allPokemonsList(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('All Pokemons', style: TextStyle(fontSize: 25)),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.60,
+            child: ListView.builder(
+              itemCount: 0,
+              itemBuilder: (context, index) {
+                return const ListTile();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
