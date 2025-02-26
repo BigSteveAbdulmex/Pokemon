c7c5b3e4bb287416d0c6b3a9d1654c4e911cee09
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-26 22:15:38 +0100

    saved the databse to device storage

--- lib/providers/pokemon_data_providers.dart
+++ lib/providers/pokemon_data_providers.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
 
 import 'package:pokemon/models/pokemon.dart';
+import 'package:pokemon/services/database_service.dart';
 import 'package:pokemon/services/http_service.dart';
 
 final pokemonDataProvider = FutureProvider.family<Pokemon?, String>((
@@ -23,17 +24,28 @@ final favoritePokemonProvider =
     });
 
 class FavoritePokemonProvider extends StateNotifier<List<String>> {
+  final DatabaseService _databaseService =
+      GetIt.instance.get<DatabaseService>();
+
+  String FAVORITE_POKEMON_LIST_KEY = "FAVORITE_POKEMON_LIST_KEY";
   FavoritePokemonProvider(super._state) {
     _setup();
   }
 
-  Future<void> _setup() async {}
+  Future<void> _setup() async {
+    List<String>? result = await _databaseService.getList(
+      FAVORITE_POKEMON_LIST_KEY,
+    );
+    state = result ?? [];
+  }
 
   void addFavoritePokemon(String url) {
     state = [...state, url];
+    _databaseService.saveList(FAVORITE_POKEMON_LIST_KEY, state);
   }
 
   void removeFavoritePokemon(String url) {
     state = state = state.where((e) => e != url).toList();
+    _databaseService.saveList(FAVORITE_POKEMON_LIST_KEY, state);
   }
 }
