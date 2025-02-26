cd4709f5981e91784a215eaa59e5937223bb456f
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-26 23:20:13 +0100

    Styling the pokemon card

--- lib/widgets/pokemon_card.dart
+++ lib/widgets/pokemon_card.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:pokemon/models/pokemon.dart';
+import 'package:pokemon/providers/pokemon_data_providers.dart';
 
 class PokemonCard extends ConsumerWidget {
   final String pokemonURL;
@@ -7,5 +11,64 @@ class PokemonCard extends ConsumerWidget {
   const PokemonCard({super.key, required this.pokemonURL});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pokemon = ref.watch(pokemonDataProvider(pokemonURL));
+    return pokemon.when(
+      data: (data) {
+        return _card(context, false, data);
+      },
+      error: (error, stackTrace) {
+        return Text('Error $error');
+      },
+      loading: () {
+        return _card(context, true, null);
+      },
+    );
+  }
+
+  Widget _card(BuildContext context, bool isLoading, Pokemon? pokemon) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: MediaQuery.sizeOf(context).width * 0.03,
+        vertical: MediaQuery.sizeOf(context).height * 0.01,
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.sizeOf(context).width * 0.03,
+        vertical: MediaQuery.sizeOf(context).height * 0.01,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Theme.of(context).primaryColor,
+        boxShadow: const [
+          BoxShadow(color: Colors.black26, spreadRadius: 2, blurRadius: 10),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                pokemon?.name?.toUpperCase() ?? "Pokemon",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "#${pokemon?.id?.toString()}",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
